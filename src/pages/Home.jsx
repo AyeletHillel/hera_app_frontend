8e615500b29c716f232b5ec64335754a69cd24f2
Ayelet Hillel <ah3708@tc.columbia.edu>
2023-01-28 21:12:19 -0500

    initial setup and navbar

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+
+//rafce
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home
+
