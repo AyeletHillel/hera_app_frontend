4b181710d60a17d88a70d2d67efd1d1645d2ec31
Ayelet Hillel <ah3708@tc.columbia.edu>
2023-01-30 18:46:09 -0500

    added Register and Login pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import './App.css';
 import Home from './pages/Home';
 import ProductList from './pages/ProductList';
 import Product from './pages/Product';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
-  return <Product/>;
+  return <Login/>;
 }
 
 export default App;
