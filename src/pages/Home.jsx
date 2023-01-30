d39f7d48cdfab9e3007cf437d7ef0b3c117554e1
Ayelet Hillel <ah3708@tc.columbia.edu>
2023-01-30 15:08:34 -0500

    added Categories and CategoryItem

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Announcement from '../components/Announcement'
 import Slider from '../components/Slider'
+import Categories from '../components/Categories'
 
 
 const Home = () => {
@@ -12,6 +13,7 @@ const Home = () => {
       <Announcement/>
       <Navbar/>
       <Slider/>
+      <Categories/>
     </div>
   )
 }
